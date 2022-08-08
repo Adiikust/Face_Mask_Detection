156018a6cbdf1f0ce4f9af3e0f02a10e47207a33
unknown <adii.developer277@gmail.com>
2022-08-07 22:17:25 -0700

    second commit

--- lib/Controller/export/export_all.dart
+++ lib/Controller/export/export_all.dart
@@ -12,3 +12,19 @@ export 'package:untitled1/Views/auth/forgot_screen.dart';
 
 
 //Controller
+
+
+// pkg
+//import 'package:auto_size_text/auto_size_text.dart';
+//AutoSizeText(
+//'This string will be automatically resized to fit in two lines.',
+//style: TextStyle(fontSize: 30),
+//maxLines: 2,
+
+//import 'package:firebase_auth/firebase_auth.dart';
+
+//import 'package:form_field_validator/form_field_validator.dart';
+
+//import 'package:image_picker/image_picker.dart';
+
+//import 'package:introduction_screen/introduction_screen.dart';
---
+++ lib/Views/splash_screen/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Scaffold(
+      body: SizedBox(
+        height: 20,
+        width: 20,
+      )
+
+    );
+  }
+}
